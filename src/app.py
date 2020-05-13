880f20a07c115f8e7971be9aa2f6483c4588c555
Alejandro Sanchez <aalejo@gmail.com>
2020-05-13 22:13:47 +0000

    family tree static

--- src/app.py
+++ src/app.py
@@ -11,6 +11,8 @@ from datastructures import FamilyStructure
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 CORS(app)
+
+# create the jackson family object
 jackson_family = FamilyStructure("Jackson")
 
 # Handle/serialize errors like a JSON object
@@ -24,41 +26,19 @@ def sitemap():
     return generate_sitemap(app)
 
 @app.route('/members', methods=['GET'])
-def get_all_members():
+def handle_hello():
+
+    # this is how you can use the Family datastructure by calling its methods
     members = jackson_family.get_all_members()
-    return jsonify(members), 200
-
-@app.route('/member/<int:id>', methods=['GET'])
-def get_single_member(id):
-    member = jackson_family.get_member(id)
-    return jsonify(member), 200
-
-@app.route('/member', methods=['POST'])
-def create_member():
-    member = request.json
-    print("add", member)
-    jackson_family.add_member(member)
-    return "", 200
-
-@app.route('/member/<int:id>', methods=['DELETE'])
-def delete_single_member(id):
-    members = jackson_family.delete_member(id)
-    return "", 200
-
-# @app.route('/members', methods=['GET'])
-# def handle_hello():
-
-#     # this is how you can use the Family datastructure by calling its methods
-#     members = jackson_family.get_all_members()
-#     response_body = {
-#         "hello": "world",
-#         "family": members
-#     }
+    response_body = {
+        "hello": "world",
+        "family": members
+    }
 
 
-#     return jsonify(response_body), 200
+    return jsonify(response_body), 200
 
-# this only runs if `$ python src/main.py` is executed
+# this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
     app.run(host='0.0.0.0', port=PORT, debug=True)
