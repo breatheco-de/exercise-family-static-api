210411a8a51ed52582caadf41795b0a88c42bb99
Andleus <abadiaandres@gmail.com>
2024-01-08 21:09:25 +0100

    FamilyApi DONE

--- src/app.py
+++ src/app.py
@@ -13,7 +13,7 @@ app.url_map.strict_slashes = False
 CORS(app)
 
 # create the jackson family object
-jackson_family = FamilyStructure("Jackson")
+pets_family = FamilyStructure("Pets")
 
 # Handle/serialize errors like a JSON object
 @app.errorhandler(APIException)
@@ -25,18 +25,70 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/members', methods=['GET'])
-def handle_hello():
+@app.route('/members', methods=['GET', 'POST'])
+def handle_members():
 
-    # this is how you can use the Family datastructure by calling its methods
-    members = jackson_family.get_all_members()
-    response_body = {
-        "hello": "world",
-        "family": members
-    }
+    if request.method == "GET":
+        members = pets_family.get_all_members()
+        response_body = {
+            "pets-family":members
+        }
+        return jsonify(response_body), 200
+    
+    elif request.method == "POST":
+        try:
+            new_member_data = request.json
+
+            print("ESTO ES REQUEST ->",request.json)
+            for member in new_member_data:
+                pets_family.add_member(member)
+
+
+            return jsonify(new_member_data), 201
+        
+        except Exception as e:
+            raise APIException(str(e), status_code=400)
 
 
     return jsonify(response_body), 200
+
+
+
+@app.route('/members/<int:member_id>', methods=["DELETE", "PUT", "GET"])
+def handle_delete_member(member_id):
+
+    if request.method == "PUT":
+        try:
+            updated_data = request.json
+            updated_member = pets_family.update_member(member_id, updated_data)
+            return jsonify(updated_member), 200
+        
+        except ValueError as ve:
+            raise APIException(str(ve), status_code=404)
+        
+    elif request.method == "GET":
+        try:
+            get_member = pets_family.get_member(member_id)
+            return jsonify(get_member), 200
+        
+        except ValueError as ve:
+            raise APIException(str(ve), status_code=404)
+
+
+    elif request.method == "DELETE":
+
+        try:
+            deleted_member = pets_family.delete_member(member_id)
+            return jsonify(deleted_member), 200
+        
+        except ValueError as ve:
+            raise APIException(str(ve), status_code=404)
+
+
+
+
+
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/datastructures.py
+++ src/datastructures.py
@@ -15,22 +15,57 @@ class FamilyStructure:
         # example list of members
         self._members = []
 
+
     # read-only: Use this method to generate random members ID's when adding members into the list
     def _generateId(self):
         return randint(0, 99999999)
 
-    def add_member(self, member):
-        # fill this method and update the return
-        pass
-
-    def delete_member(self, id):
-        # fill this method and update the return
-        pass
-
-    def get_member(self, id):
-        # fill this method and update the return
-        pass
-
     # this method is done, it returns a list with all the family members
     def get_all_members(self):
         return self._members
+
+    def get_member(self, member_id):
+        # fill this method and update the return
+
+        selected_member = next((member for member in self._members if member['id'] == member_id), None)
+
+        if selected_member:
+            return selected_member
+        else:
+            raise ValueError(f"The id {member_id} couldn't be found")
+    
+    def add_member(self, member_data):
+        # fill this method and update the return
+        new_member = {
+            "id": self._generateId(),
+            "name": member_data['name'],
+            "age": member_data['age'],
+            "lucky_numbers": member_data['lucky_numbers']
+        }
+
+        self._members.append(new_member)
+        return new_member
+
+
+    def delete_member(self, member_id):
+        # fill this method and update the return
+
+        deleted_member = next((member for member in self._members if member['id'] == member_id), None)
+
+        if deleted_member:
+            self._members.remove(deleted_member)
+            return deleted_member
+        else:
+            raise ValueError(f"The id {member_id} couldn't be found")
+        
+
+    def update_member(self, member_id, updated_member_data):
+    # fill this method and update the return
+
+        updated_member = next((member for member in self._members if member['id'] == member_id), None)
+
+        if updated_member:
+            updated_member.update(updated_member_data)
+            return updated_member
+        else:
+            raise ValueError(f"The id {member_id} couldn't be found")
