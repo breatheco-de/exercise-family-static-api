66162a9d105093c05fe4b1d9e6f160d77be99f20
Matteo0310 <145265011+Matteo0310@users.noreply.github.com>
2024-02-17 12:30:01 +0000

    "Excercise done"

--- src/app.py
+++ src/app.py
@@ -6,39 +6,72 @@ from flask import Flask, request, jsonify, url_for
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from datastructures import FamilyStructure
-#from models import Person
+# from models import Person
+
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 CORS(app)
+jackson_family = FamilyStructure("Jackson")  # Create the jackson family object
 
-# create the jackson family object
-jackson_family = FamilyStructure("Jackson")
 
 # Handle/serialize errors like a JSON object
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
 
-# generate sitemap with all your endpoints
+
+# Generate sitemap with all your endpoints
 @app.route('/')
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/members', methods=['GET'])
+
+"""
+CRUD
+'/members' GET - devuelve TODOS los integrantes
+'/members' POST - Crea UNO integrantes
+'/members/<int:id_member>' GET - devuelve UNO integrante
+'/members/<int:id_member>' PUT - modifica UNO integrante
+'/members/<int:id_member>' DELETE - borra UNO integrante
+"""
+
+
+@app.route('/members', methods=['GET', 'POST'])
 def handle_hello():
+    response_body = {}
+    # This is how you can use the Family datastructure by calling its methods
+    if request.method == 'GET':
+        members = jackson_family.get_all_members()
+        response_body["hello"] = "world"
+        response_body["family"] = members
+        return response_body, 200
+    if request.method == 'POST':
+        data = request.json  # Recibo los datos del front (body)
+        #  Ejecuto el método para agregar el member en la lista
+        results = jackson_family.add_member(data)
+        response_body['message'] = 'Miembro agregado'
+        response_body['results'] = results
+        return response_body, 200
 
-    # this is how you can use the Family datastructure by calling its methods
-    members = jackson_family.get_all_members()
-    response_body = {
-        "hello": "world",
-        "family": members
-    }
+
+@app.route('/members/<int:id>', methods=['GET', 'DELETE'])
+def handle_member(id):
+    if request.method == 'GET':
+        results = jackson_family.get_member(id)
+        if results == []:
+            response_body = {'message': 'No encontrado'}
+            return  response_body, 405
+        response_body = {'member': results}
+        return response_body, 200
+    if request.method == 'DELETE':
+        results = jackson_family.delete_member(id)
+        response_body = {'message': 'Eliminado',
+                         'results': results}
+        return response_body, 200
 
 
-    return jsonify(response_body), 200
-
-# this only runs if `$ python src/app.py` is executed
+# This only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
     app.run(host='0.0.0.0', port=PORT, debug=True)
--- src/datastructures.py
+++ src/datastructures.py
@@ -1,4 +1,3 @@
-
 """
 update this file to implement the following already declared methods:
 - add_member: Should add a member to the self._members list
@@ -8,29 +7,60 @@ update this file to implement the following already declared methods:
 """
 from random import randint
 
+
 class FamilyStructure:
     def __init__(self, last_name):
         self.last_name = last_name
+        # Defino los tres miembros iniciales
+        self._members = [{'first_name': 'John',
+                          'last_name': self.last_name,
+                          'age': 33,
+                          'lucky_numbers': [7, 13, 22],
+                          'id': self._generate_id()},
+                         {'first_name': 'Jane',
+                          'last_name': self.last_name,
+                          'age': 35,
+                          'id': self._generate_id(),
+                          'lucky_numbers': [10, 14, 3]},
+                         {'first_name': 'Jimmy',
+                          'last_name': self.last_name,
+                          'age': 5,
+                          'id': self._generate_id(),
+                          'lucky_numbers': [1]}]
 
-        # example list of members
-        self._members = []
 
-    # read-only: Use this method to generate random members ID's when adding members into the list
-    def _generateId(self):
+    # Read-only: Use this method to generate random members ID's when adding members into the list
+    def _generate_id(self):
         return randint(0, 99999999)
 
     def add_member(self, member):
-        # fill this method and update the return
-        pass
+        # Agrego las claves id y last_name al dict member que recibí
+        member['id'] = self._generate_id()
+        member['last_name'] = self.last_name
+        # Agregar member a la lista self._members
+        self._members.append(member)
+        # Fill this method and update the return
+        return self._members
 
     def delete_member(self, id):
-        # fill this method and update the return
-        pass
+        self._members = [item for item in self._members if id != item['id']]
+        return self._members
 
     def get_member(self, id):
-        # fill this method and update the return
-        pass
+        # Recorrer la lista buscando el id, devolver el miembro que hemos encontrado
+        # Opcion 1
+        """         
+        for item in self._members:
+            if id == item['id']:
+                return {'results': item} 
+        return {'message': 'No encontrado'}
+        """
+        # opcion 2 - list comprehension
+        # variable = objetivo recorriendo una lista y filtrando por una condición
+        results = [item for item in self._members if id == item['id']]
+        return results
+        
 
-    # this method is done, it returns a list with all the family members
+    # This method is done, it returns a list with all the family members
     def get_all_members(self):
         return self._members
