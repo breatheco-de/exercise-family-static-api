3e5e1ef3e4317ab6b9c1c7f823e0044d83450f63
Ignacio Garfias <153826168+ignaciofrag@users.noreply.github.com>
2024-03-22 23:01:04 +0000

    entrega

--- src/app.py
+++ src/app.py
@@ -26,17 +26,49 @@ def sitemap():
     return generate_sitemap(app)
 
 @app.route('/members', methods=['GET'])
-def handle_hello():
-
-    # this is how you can use the Family datastructure by calling its methods
+def get_all_members():
     members = jackson_family.get_all_members()
-    response_body = {
-        "hello": "world",
-        "family": members
+    return jsonify(members), 200
+
+@app.route('/member/<int:member_id>', methods=['GET'])
+def get_member(member_id):
+    member = jackson_family.get_member(member_id)
+    if member:
+        return jsonify(member), 200
+    else:
+        return jsonify({"error": "Member not found"}), 404
+
+#AQUÍ AGREGA UN NUEVO MIEMBRO A LA FAMILIA
+@app.route('/member', methods=["POST"])
+def create_member():
+    first_name = request.json.get("first_name")
+    age = request.json.get("age")
+    lucky_numbers = request.json.get("lucky_numbers")
+
+    if not first_name or not isinstance(age, int) or not isinstance(lucky_numbers, list) or age <= 0:
+        return jsonify({"error": "Invalid data provided"}), 400
+
+    member = {
+        "first_name": first_name,
+        "age": age,
+        "lucky_numbers": lucky_numbers,
+        "last_name": jackson_family.last_name, 
+        "id": jackson_family._generateId()
     }
 
+    jackson_family.add_member(member)
+    return jsonify(member), 200
 
-    return jsonify(response_body), 200
+# ElimiNA MIEMBRO X ID
+@app.route('/member/<int:member_id>', methods=['DELETE'])
+def delete_member(member_id):
+    success = jackson_family.delete_member(member_id)
+    if success:
+        return jsonify({"done": True}), 200
+    else:
+        return jsonify({"error": "Member not found"}), 404
+
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/datastructures.py
+++ src/datastructures.py
@@ -13,7 +13,25 @@ class FamilyStructure:
         self.last_name = last_name
 
         # example list of members
-        self._members = []
+        self._members = [{
+            "id": self._generateId(),
+            "first_name": "John",
+            "last_name": last_name,
+            "age": 33,
+            "lucky_numbers": [7, 13, 22]
+        }, {
+            "id": self._generateId(),
+            "first_name": "Jane",
+            "last_name": last_name,
+            "age": 35,
+            "lucky_numbers": [10, 14, 3]
+        }, {
+            "id": self._generateId(),
+            "first_name": "Jimmy",
+            "last_name": last_name,
+            "age": 5,
+            "lucky_numbers": [1]
+        }]
 
     # read-only: Use this method to generate random members ID's when adding members into the list
     def _generateId(self):
@@ -21,15 +39,25 @@ class FamilyStructure:
 
     def add_member(self, member):
         # fill this method and update the return
-        pass
+         member["id"] = self._generateId()
+         self._members.append(member)
+        
 
     def delete_member(self, id):
         # fill this method and update the return
-        pass
+         for i, member in enumerate(self._members):
+            if member["id"] == id:
+                return self._members.pop(i)
+         return None
+        
 
     def get_member(self, id):
         # fill this method and update the return
-        pass
+        for member in self._members:
+            if member["id"] == id:
+                return member
+        return None
+        
 
     # this method is done, it returns a list with all the family members
     def get_all_members(self):
