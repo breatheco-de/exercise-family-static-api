e13474d965b719bd454980fed823862b69f8c0b3
EduardoLoreto <138321528+EduardoLoreto@users.noreply.github.com>
2024-06-28 13:18:07 +0000

    Avances en DataStructures

--- src/datastructures.py
+++ src/datastructures.py
@@ -11,9 +11,28 @@ from random import randint
 class FamilyStructure:
     def __init__(self, last_name):
         self.last_name = last_name
+               
 
         # example list of members
-        self._members = []
+        self._members = [{"id":self._generateId(),
+                          "first_name":"John",
+                          "last_name": self.last_name,
+                          "age": 33,
+                          "lucky_numbers": [7, 13, 22]},
+
+                          {"id":self._generateId(),
+                          "first_name":"Jane",
+                          "last_name": self.last_name,
+                          "age": 35,
+                          "lucky_numbers": [10, 14, 3]},
+                          
+                          {"id":self._generateId(),
+                          "first_name":"Jimmy",
+                          "last_name": self.last_name,
+                          "age": 5,
+                          "lucky_numbers": [1]
+    }]
+        
 
     # read-only: Use this method to generate random members ID's when adding members into the list
     def _generateId(self):
@@ -21,8 +40,13 @@ class FamilyStructure:
 
     def add_member(self, member):
         # fill this method and update the return
-        pass
-
+          self._members.append({
+            "first_name" : member['first_name'],
+            "id": member['id'],
+            "age": member['age'],
+            "lucky_numbers" : member['lucky_numbers']
+        })
+          
     def delete_member(self, id):
         # fill this method and update the return
         pass
